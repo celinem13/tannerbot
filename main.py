4c1df233885f8da30da19b04fd1d8ebd70b22151
celinem13 <celinemangahas13@gmail.com>
2023-04-01 02:09:26 -0700

    Update main.py
    Using asyncpraw, asyncio, discord, await, reddit json, and aiohttp instead of requests, praw, and time.sleep

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import time
-
 import discord
 import os
-import requests
 import json
 import random
-import praw
-
+import aiohttp
+import asyncpraw
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -21,7 +19,7 @@ password = os.environ['your_reddit_password']
 client_secret = os.environ['reddit_client_secret']
 user_agent = os.environ['reddit_user_agent']
 
-reddit = praw.Reddit(client_id=client_id,
+reddit = asyncpraw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      username="tannerbot227",
                      password=password,
@@ -49,11 +47,12 @@ else:
     encouragements = starter_encouragements
 
 
-def get_quote():
-    response = requests.get("https://zenquotes.io/api/random")
-    json_data = json.loads(response.text)
-    quote = json_data[0]["q"] + " -" + json_data[0]["a"]
-    return quote
+async def get_quote():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://zenquotes.io/api/random") as response:
+            json_data = await response.json()
+            quote = json_data[0]["q"] + " -" + json_data[0]["a"]
+            return quote
 
 
 def update_encouragements(encouraging_message):
@@ -70,7 +69,6 @@ def delete_encouragement(index):
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
-
 
 @client.event
 async def on_message(message):
@@ -112,25 +110,29 @@ async def on_message(message):
             await message.channel.send("Responding is off.")
 
     if msg.startswith('$meme'):
-        subreddit = reddit.subreddit('memes')
-        posts = subreddit.hot(limit=50)
-        random_post_number = random.randint(1, 50)
-        for i, post in enumerate(posts):
-            if i == random_post_number:
-                title = post.title
-                image_url = post.url
-                embed = discord.Embed(title=title)
-                embed.set_image(url=image_url)
-                await message.channel.send(embed=embed)
-                break
+        async with aiohttp.ClientSession() as session:
+            subreddit = await reddit.subreddit('memes')
+            async for post in subreddit.hot(limit=50):
+                random_post_number = random.randint(1, 50)
+                if post.stickied:
+                    continue
+                data = {"title": post.title, "url": post.url}
+                if data["url"].endswith(('.jpg', '.jpeg', '.png')):
+                    title = data['title']
+                    image_url = data['url']
+                    embed = discord.Embed(title=title)
+                    embed.set_image(url=image_url)
+                    await message.channel.send(embed=embed)
+                    break
 
     if msg.startswith('$joke'):
-        response = requests.get("https://official-joke-api.appspot.com/random_joke")
-        json_data = json.loads(response.text)
-        setup = json_data["setup"]
-        punchline = json_data["punchline"]
-        await message.channel.send(setup)
-        time.sleep(3)
-        await message.channel.send(punchline)
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://official-joke-api.appspot.com/random_joke") as response:
+                data = await response.json()
+                setup = data["setup"]
+                punchline = data["punchline"]
+                await message.channel.send(setup)
+                await asyncio.sleep(3)
+                await message.channel.send(punchline)
 
 client.run(os.getenv("TOKEN"))

ada77a293b520df9c2e6299e1ee611e126b517c2
celinem13 <celinemangahas13@gmail.com>
2023-04-07 22:44:30 -0700

    Update main.py
    Fixing play command syntax

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
 
 @bot.command()
-async def play(ctx, *, url):
+async def play(ctx, *url):
     queue.append(url)
     await play_song(ctx, queue[0])
 
